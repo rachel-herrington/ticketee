e68626acd8834a1915d21acef2c29b71f65d9c19
Rachel Herrington <rachelhelotes@gmail.com>
2016-02-19 19:17:35 -0500

    Restrict tagging to only authorized users and remove unnecessary repo files

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
 
     context "for viewers of the project" do
@@ -25,6 +26,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :update}
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
 
     context "for editors of the project" do
@@ -34,6 +36,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
 
       context "when the editor created the ticket" do
         before { ticket.author = user }
@@ -49,6 +52,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :update }
       it { should permit_action :destroy }
       it { should permit_action :change_state }
+      it { should permit_action :tag }
     end
 
     context "for managers of other projects" do
@@ -61,6 +65,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
 
     context "for administrators" do
@@ -70,6 +75,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :update }
       it { should permit_action :destroy }
       it { should permit_action :change_state }
+      it { should permit_action :tag }
     end
   end
 end
