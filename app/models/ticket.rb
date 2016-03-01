3e1d22d6f2f39d8a4a33975b4f6581d9fc6fa1a1
Rachel Herrington <rachelhelotes@gmail.com>
2016-03-01 17:57:31 -0500

    Added basic email ticket notifications

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,9 +4,12 @@ class Ticket < ActiveRecord::Base
   belongs_to :state
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_and_belongs_to_many :watchers, join_table: "ticket_watchers",
+    class_name: "User", uniq: true
   has_and_belongs_to_many :tags, uniq: true
   attr_accessor :tag_names
   before_create :assign_default_state
+  after_create :author_watches_me
 
   searcher do
     label :tag, from: :tags, field: "name"
@@ -28,5 +31,11 @@ class Ticket < ActiveRecord::Base
 
   def assign_default_state
   	self.state ||= State.default
+  end
+
+  def author_watches_me
+    if author.present? && !self.watchers.include?(author)
+      self.watchers << author
+    end
   end
 end
