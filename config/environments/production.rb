f804bf792d33dee8dc7cabcba767ea99ff73c436
Rachel Herrington <rachelhelotes@gmail.com>
2016-03-14 13:36:49 -0400

    Add Mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,23 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ActionMailer::Base.delivery_method = :smtp
+
+  host = "still-reef-61780.herokuapp.com"
+
+  ActionMailer::Base.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         host,
+    authenticatoin: :plain,
+  }
+
+  config.action_mailer.default_url_options = {
+    host: host
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
